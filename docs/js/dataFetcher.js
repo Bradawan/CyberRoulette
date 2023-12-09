da7a2f0f168c1a43b1504a2453c95b18cb58e9e7
Brad <101075705+Bradawan@users.noreply.github.com>
2023-12-09 05:34:17 -0700

    Update dataFetcher.js
    first code upload for data fetcher, cant wait to debug for 3 hours

--- docs/js/dataFetcher.js
+++ docs/js/dataFetcher.js
@@ -1,1 +1,22 @@
-placeholder
+function fetchRandomLine(filePath, callback) {
+  fetch(filePath)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.text();
+    })
+    .then(text => {
+      const lines = text.split('\n').filter(line => line.trim());
+      if (lines.length === 0) {
+        throw new Error('The file is empty or does not contain valid lines.');
+      }
+      const randomIndex = Math.floor(Math.random() * lines.length);
+      const randomLine = lines[randomIndex].trim();
+      callback(randomLine);
+    })
+    .catch(error => {
+      console.error('Failed to fetch random line:', error);
+      callback(null, error);
+    });
+}
