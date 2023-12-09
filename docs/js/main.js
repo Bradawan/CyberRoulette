8bb385029f49412a3d3ee54a97150d01050aae23
Brad <101075705+Bradawan@users.noreply.github.com>
2023-12-09 05:35:08 -0700

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,1 +1,16 @@
-placeholder
+function handleRandomLine(line, error) {
+  const outputElement = document.getElementById('output');
+  if (error) {
+    outputElement.textContent = 'Sorry, something went wrong.';
+  } else {
+    outputElement.textContent = line;
+  }
+}
+
+document.getElementById('random-username-btn').addEventListener('click', function() {
+  fetchRandomLine('data/usernames.txt', handleRandomLine);
+});
+
+document.getElementById('random-password-btn').addEventListener('click', function() {
+  fetchRandomLine('data/passwords.txt', handleRandomLine);
+});
